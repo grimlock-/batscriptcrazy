d17cf72c88a0842856409f1745bf70d26a054dab
Grimlock <gtm.mason@gmail.com>
2015-11-05 23:43:26 -0500

    Bugs and zip file automator

--- pullFiles.bat
+++ pullFiles.bat
@@ -1,16 +1,39 @@
 @echo off
 REM Grab the contents of every directory in the current one or in the directory specified
 REM by argument one and move them up on directory
-Setlocal EnableDelayedExpansion
+setlocal EnableDelayedExpansion
 if errorlevel 1 (
 	echo Unable to enable Delayed variable expansion
 	exit /B
 )
 
 set target=*
+set debug=no
 
-if not -%1-==-- set target=%1
+for %%i in (%*) do (
+	if %%i==--debug (
+		set debug=yes
+	) else if %%i==-d (
+		set debug=yes
+	) else (
+	    set target=%%i
+	)
+)
+
+if !debug!==yes (
+    echo Target directory: !target!
+)
+
 for /D %%i in (!target!) do (
-    move "%%i\..\*" "%%i\.."
+    if !debug!==yes (
+	    set tmp=%%i
+        echo move "!tmp!\*" "!tmp!\.."
+    ) else (
+	    move "%%i\*" "%%i\.."
+	)
 )
 exit /B
+
+::BUGS
+::When a target's only contents are more directories, this error is generated
+::"The filename, directory name, or volume label syntax is incorrect."
---
+++ xzips.bat
@@ -0,0 +1,98 @@
+@echo off
+REM Extract the contents of every zip file in the current directory into
+REM seperate directories
+setlocal EnableDelayedExpansion
+if errorlevel 1 (
+	echo Unable to enable Delayed variable expansion
+	exit /B
+)
+
+set debug=no
+set destination=%~dp0
+set single_dir=no
+set _slash=\
+
+if -%1-==-- goto loopend
+
+:argloop
+if %1==--debug (
+	set debug=yes
+) else if %1==-d (
+	set debug=yes
+) else if %1==--single-directory (
+	set single_dir=yes
+) else if %1==-sd (
+	set single_dir=yes
+) else if %1==--output-directory (
+	set destination=%~2
+	shift
+) else if %1==-od (
+	set destination=%~2
+	shift
+) else if %1==-h (
+	goto printHelp
+) else if %1==--help (
+	goto printHelp
+)
+shift
+if not -%1-==-- goto argloop
+
+REM Add trailing slash if necessary
+if not !destination:~-1!==\ (
+	if not !destination:~-1!==/ (
+		set destination=!destination!\
+	)
+)
+
+REM Situation: execute "xzips.bat -sd"
+REM A new directory should be created to hold the contents of all the archives
+REM since extracting everything to the current directory might clutter it up.
+REM An "extract all here" can be done with "xzips.bat -sd -od ."
+if !single_dir!==yes (
+	if "!destination!" == "%~dp0" (
+		set destination=!destination!archives\
+	)
+)
+
+:loopend
+for %%i in (*) do (
+	if %%~xi==.zip (
+		if !single_dir!==no (
+			set tmp_dst=!destination!%%~ni
+			if !debug!==yes (
+				echo 7z.exe x -r -o"!tmp_dst!" %%i
+			) else (
+				call 7z.exe x -r -o"!tmp_dst!" %%i
+			)
+		) else (
+			if !debug!==yes (
+				echo 7z.exe x -r -o"!destination!" %%i
+			) else (
+				call 7z.exe x -r -o"!destination!" %%i
+			)
+		)
+	)
+)
+exit /B
+
+:printHelp
+echo Extracts the contents of every zip file in the current directory
+echo.
+echo xzips [options]
+echo.
+echo   -d, --debug
+echo       Don't extract any archives, print the command to the console instead
+echo   -sd, --single-directory
+echo       Extract all archives to the same directory
+echo   -od dir (--output-directory)
+echo       Directory to output the archive contents to
+echo   -h, --help
+echo       Print this message
+echo.
+echo If no options are specified, the contents of each zip file will be stored
+echo in their own new directory named after the archive.
+echo The -sd option will consolidate the contents of all archives into just the
+echo output directory (current directory if not specified). If -sd is the only
+echo option specified a new directory will be created to hold the archives'
+echo contents.
+echo "xzips -sd -od ." can be used to store everything in the current directory
