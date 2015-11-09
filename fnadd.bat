c550f68a0c83a4b66401da8ac7b8ce3a1aabf14e
Grimlock <gtm.mason@gmail.com>
2015-11-09 17:11:55 -0500

    Fix for extensions that aren't 3 chars long

--- fnadd.bat
+++ fnadd.bat
@@ -1,6 +1,16 @@
 @echo off
-REM This script takes a string as an argument and adds it to the filename for
-REM each file and directory in the current directory
+REM This script takes a string as an argument and adds it to the name for
+REM every file and directory in the current directory
+setlocal EnableDelayedExpansion
+if errorlevel 1 (
+	echo Unable to enable delayed variable expansion
+	exit /B
+)
+setlocal EnableExtensions
+if errorlevel 1 (
+	echo Unable to enable Command extensions
+	exit /B
+)
 if not -%1-==-- goto :main
 
 :printHelp
@@ -8,14 +18,29 @@ echo Add a string of text to each file in the current directory
 echo.
 echo fnadd [app] [options] text
 echo.
-echo   [app]    Append provided text to the end of each file instead of the
-echo            beginning
+echo   text
+echo       What to add to each filename
 echo.
 echo   OPTIONS:
-echo     -d          Don't rename anything, just print out the new filenames
-REM echo     -nd         Disable operation on directories
-REM echo     -nf         Disable operation on files
+echo     -a, --append
+echo         Append text to the end of each file instead of the beginning
+echo     -nd, --no-directories (NOT YET IMPLEMENTED)
+echo         Disable operation on directories
+echo     -nf, --no-files (NOT YET IMPLEMENTED)
+echo         Disable operation on files
+echo     -h, --hold
+echo         Don't rename anything, just print out the new filenames
+echo     -d, --debug
+echo         "hold" flag and some more verbose output
+echo     --help
+echo         Print this message
 exit /B
+
+:addToDir
+REM TODO - everything
+
+:appendToDir
+REM TODO - everything
 
 :addToFile
 set a=%~1
@@ -23,15 +48,11 @@ if !_debug!==yes (
 	echo Processing file: !a!
 	echo Adding         : !_addme!
 )
-echo !a!>x & for %%i in (x) do (
-	set /A count=%%~zi - 2 & del x
-)
-set /A count=!count!-1
-set /A fnlength=!count!-4
-
 set "newFile=!_addme!!a!"
 
 if !_debug!==yes (
+	echo New filename   : !newFile!
+) else if !_hold!==yes (
 	echo New filename   : !newFile!
 ) else (
 	REM Add a check to make sure the file exists
@@ -46,17 +67,24 @@ if !_debug!==yes (
 	echo Processing file: !a!
 	echo Appending      : !_addme!
 )
-set "ext=!a:~-4,4!"
+
+REM Get filename length and extension length
 echo !a!>x & for %%i in (x) do (
 	set /A count=%%~zi - 2 & del x
 )
 set /A count=!count!-1
-set /A fnlength=!count!-4
+echo !_ext!>x & for %%i in (x) do (
+	set /A extcount=%%~zi - 2 & del x
+)
+set /A extcount=!extcount!-1
+set /A fnlength=!count!-!extcount!
 
 call set newFile=!a:~0,%fnlength%!
-set "newFile=!newFile!!_addme!!ext!"
+set "newFile=!newFile!!_addme!!_ext!"
 
 if !_debug!==yes (
+	echo New filename   : !newFile!
+) else if !_hold!==yes (
 	echo New filename   : !newFile!
 ) else (
 	REM Add a check to make sure the file exists
@@ -66,41 +94,77 @@ echo.
 goto :eof
 
 :main
-setlocal enabledelayedexpansion
-if errorlevel 1 (
-	echo Unable to enable delayed variable expansion
-	exit /B
-)
-
 set a=
+set _self=%~nx0
 set _addme=
 set _mode=beg
 set _debug=no
+set _hold=no
 set _files=yes
 set _directories=yes
 
-for %%i in (%*) do (
-	if %%i==app (
-		set _mode=end
-	) else if %%i==-d (
-		set _debug=yes
-	) else if %%i==-nf (
-		set _files=no
-	) else if %%i==-nd (
-		set _directories=no
-	) else if not %%i==%0 (
-		set _addme=%%~i
+:argloop
+if %1==-a (
+	set _mode=end
+) else if %1==--append (
+	set _mode=end
+) else if %1==-d (
+	set _debug=yes
+) else if %1==--debug (
+	set _debug=yes
+) else if %1==-nf (
+	set _files=no
+) else if %1==-no-files (
+	set _files=no
+) else if %1==-nd (
+	set _directories=no
+) else if %1==--no-directories (
+	set _directories=no
+) else if %1==-h (
+	set _hold=yes
+) else if %1==--hold (
+	set _hold=yes
+) else if %1==--help (
+	goto printHelp
+) else (
+	set _addme=%%~i
+)
+shift
+if not -%1-==-- goto argloop
+
+REM sanity check
+if -!_addme!-==-- goto printHelp
+
+if !_debug!==yes (
+	if !_mode!==end (
+		echo Add to     : End
+	) else (
+		echo Add to     : Beginning
 	)
+	if !_files!==no (
+		echo Files      : NO
+	) else (
+		echo Files      : YES
+	)
+	if !_directories!==no (
+		echo Directories: NO
+	) else (
+		echo Directories: NO
+	)
+	echo Text to Add: !_addme!
 )
 
-if -!_addme!-==-- goto :printHelp
-
 for %%i in (*) do (
-	if not %%i==%0 (
+	if not %%i==!_self! (
+		set attributes=%%~ai
+		set dir_attr=!attributes:~0,1!
+		if /I "!dir_attr!"=="d" echo %%i is a directory
+		
+		set _ext=%%~xi
 		if !_mode!==beg (
-			call :addToFile "%%i"
+			call addToFile "%%i"
 		) else if !_mode!==end (
-			call :appendToFile "%%i"
+			call appendToFile "%%i"
 		)
 	)
 )
--- fnchop.bat
+++ fnchop.bat
@@ -1,26 +1,51 @@
 @echo off
 REM This Script removes the first X characters from every filename in the
 REM the current directory where X is a user provided integer
-
-if not -%1-==-- (
-	goto :main
+setlocal EnableDelayedExpansion
+if errorlevel 1 (
+	echo Unable to enable Delayed variable expansion
+	exit /B
 )
+setlocal EnableExtensions
+if errorlevel 1 (
+	echo Unable to enable Command extensions
+	exit /B
+)
+if not -%1-==-- goto :main
 
-:outHelp
-echo Removes X characters from all filenames in the current directory
+:printHelp
+echo Removes num characters from all filenames in the current directory
 echo.
-echo fnchop [end] [debug] number
+echo fnchop [options] num
 echo.
-echo   [end]    Removes characters from end of filename instead of beginning
-echo   [debug]  Don't rename anything, just print out the new filenames
-echo   number   Specifies how many characters to chop off
+echo   num
+echo       Specifies how many characters to chop off
+echo.
+echo   OPTIONS
+echo     -e, --end
+echo         Removes characters from end of filename instead of beginning
+echo     -nd, --no-directories (NOT YET IMPLEMENTED)
+echo         Disable operation on directories
+echo     -nf, --no-files (NOT YET IMPLEMENTED)
+echo         Disable operation on files
+echo     -h, --hold
+echo         Don't rename anything, just print out the new filenames
+echo     -d, --debug
+echo         "hold" flag and some more verbose output
+echo     --help
+echo         Print this message
 exit /B
 
+:procDir
+REM TODO - everything
+
+:rprocDir
+REM TODO - everything
 
 :procFile
 set a=%~1
-if(!_debug!==yes) (
-	echo !a!
+if !_debug!==yes (
+	echo file:         !a!
 )
 
 echo !a!>x & for %%i in (x) do (
@@ -38,7 +63,9 @@ if !_qt! GEQ !_fnlength! (
 
 call set "_newName=!a:~%_qt%!"
 if !_debug!==yes (
-	echo !_newName!
+	echo new filename: !_newName!
+) else if !_hold!==yes (
+	echo new filename: !_newName!
 ) else (
 	REM Add a check to make sure the file exists
 	rename "!a!" "!_newName!"
@@ -53,24 +80,29 @@ goto :eof
 
 :rprocFile
 set a=%~1
-if(!_debug!==yes) (
-	echo !a!
+if !_debug!==yes (
+	echo file:         !a!
+	echo extension:    !_ext!
 )
 
+REM Get filename length and extension length
 echo !a!>x & for %%i in (x) do (
 	set /A count=%%~zi - 2 & del x
 )
 set /A count=!count!-1
-set /A _fnlength=!count!-4
+echo !_ext!>x & for %%i in (x) do (
+	set /A extcount=%%~zi - 2 & del x
+)
+set /A extcount=!extcount!-1
+set /A "_fnlength=!count!-!extcount!"
 
 if !_qt! GEQ !_fnlength! (
 	echo Cannot chop more characters than are present, skipping !a:~0,12!...
 	goto :eof
 )
 
-set "_ext=!a:~-4,4!"
 REM Back offset -> offset to use to find the substring we want to chop off
-set /A "back_ofst=4+%_qt%"
+set /A "back_ofst=%extcount%+%_qt%"
 set /A "back_ofst=0-!back_ofst!"
 
 
@@ -80,6 +112,8 @@ call set "_newName=!_newName!!_ext!"
 
 
 if !_debug!==yes (
+	echo new filename: !_newName!
+) else if !_hold!==yes (
 	echo new filename: !_newName!
 ) else (
 	REM Add a check to make sure the file exists
@@ -94,55 +128,77 @@ goto :eof
 
 
 :main
-setlocal EnableDelayedExpansion
-if errorlevel 1 (
-	echo Unable to enable Delayed variable expansion
-	exit /B
-)
-setlocal EnableExtensions
-if errorlevel 1 (
-	echo Unable to enable Command extensions
-	exit /B
-)
-
+set a=
+set _self=%~nx0
 set _mode=beg
 set _qt=0
 set _debug=no
-set a=
+set _hold=no
+set _files=yes
+set _directories=yes
 
-for %%i in (%*) do (
-	if %%i==end (
-		set _mode=end
-	) else if %%i==help (
-		goto :outHelp
-	) else if %%i==debug (
-		set _debug=yes
-	) else (
-		set _qt=%%i
-	)
+:argloop
+if %1==-e (
+	set _mode=end
+) else if %1==--end (
+	set _mode=end
+) else if %1==-h (
+	set _hold=yes
+) else if %1==--hold (
+	set _hold=yes
+) else if %1==-nf (
+	set _files=no
+) else if %1==-no-files (
+	set _files=no
+) else if %1==-nd (
+	set _directories=no
+) else if %1==--no-directories (
+	set _directories=no
+) else if %1==--d (
+	set _debug=yes
+) else if %1==--debug (
+	set _debug=yes
+) else if %1==--help (
+	goto :printHelp
+) else (
+	set _qt=%1
 )
+shift
+if not -%1-==-- goto argloop
 
 REM Test to see if _qt is a number
 REM If not, the error level will be set to 1
 echo !_qt!| findstr /r "^[1-9][0-9]*$">nul
-
 if errorlevel 1 (
 	echo Please make the last argument a number
 	exit /B
 )
 
-if(!_debug!==yes) (
-	echo Mode set to !_mode!
-	echo Quantity = !_qt!
+if !_debug!==yes (
+	if !_mode!==end (
+		echo Remove from: End
+	) else (
+		echo Remove from: Beginning
+	)
+	if !_hold!==yes (
+		echo Hold flag  : YES
+	) else (
+		echo Hold flag  : NO
+	)
+	echo Quantity   : !_qt!
 )
 
 for %%i in (*) do (
-	REM Process every file that isn't this batch file
-	if not %%i==%~nx0 (
+	if not %%i==!_self! (
+		set attributes=%%~ai
+		set dir_attr=!attributes:~0,1!
+		if /I "!dir_attr!"=="d" echo %%i is a directory
+	
+		set _ext=%%~xi
 		if !_mode!==beg (
-			call :procFile "%%i"
+			call procFile "%%i"
 		) else (
-			call :rprocFile "%%i"
+			call rprocFile "%%i"
 		)
 	)
 )
